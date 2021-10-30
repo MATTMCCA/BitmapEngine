0602d2f181429b3f2ef11e92ac3b212851563407
MATTMCCA <mccardlematthew0@gmail.com>
2021-10-30 15:32:31 -0400

    demo update
    Add some more images to test diversity.

--- Tests/ID/run_x86.bat
+++ Tests/ID/run_x86.bat
@@ -1,4 +1,6 @@
 "%CD%\x86\IDApp.exe" "Robert" "Downey Jr" "AKA IronMan" "%CD%\logo.bmp" "%CD%\rdj.bmp" "%CD%\img"
-"%CD%\x86\IDApp.exe" "Joann" "Bowers" "AKA Candy" "%CD%\logo.bmp" "%CD%\girl.bmp" "%CD%\img"
+"%CD%\x86\IDApp.exe" "Joann" "Blowers" " " "%CD%\logo.bmp" "%CD%\girl.bmp" "%CD%\img"
 "%CD%\x86\IDApp.exe" "Anibell" "Lecter" " " "%CD%\logo.bmp" "%CD%\mrsl.bmp" "%CD%\img"
+"%CD%\x86\IDApp.exe" "Samuel L." "Jackson" " " "%CD%\logo.bmp" "%CD%\slj.bmp" "%CD%\img"
+"%CD%\x86\IDApp.exe" "Some" "Guy" " " "%CD%\logo.bmp" "%CD%\asd.bmp" "%CD%\img"
 pause
